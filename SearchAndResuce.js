f4bf6f068d070dcb43b8452e330ec6840807f950
Justin <juol3453@colorado.edu>
2015-04-10 22:42:48 -0600

    first testing of grabbing the data

--- SearchAndResuce.js
+++ SearchAndResuce.js
@@ -6,11 +6,23 @@ script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';                            //import jquery
 document.getElementsByTagName('head')[0].appendChild(script);
 var url = window.location.href; //get current url
-
+var professorsOnPage = new Array();
+var i = 0;
 function grabProfessorsName() {
     if (url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART") { //check to make sure the user is on the correct page
-        iframe = $("#ptifrmtgframe").contents(); //creates a variable that references to the iframe
-        var instructorName = iframe.find("span[id*=MTG_INSTR");
+        iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
+        $.each(iframe.find("span[id*=MTG_INSTR]"),
+            function() {
+                professorsOnPage[i] = $(this).text;
+                i++;
+                //store in array
+                //modify fist last to first-last
+                //append to url "cufcq/instructors/"
+        } )
 
+        //var instructorName = iframe.find("span[id*=MTG_INSTR");
+        for(j=0;j<professorsOnPage.length;j++){
+            console.log(professorsOnPage[j]);
+        }
     }
 }
