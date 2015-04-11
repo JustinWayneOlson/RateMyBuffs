0c139ddb708ee5dfa75b8ca90be3c1a4682864f9
Justin <juol3453@colorado.edu>
2015-04-10 21:37:11 -0600

    iframe bug fix

--- SearchAndResuce.js
+++ SearchAndResuce.js
@@ -7,7 +7,10 @@ script.type = 'text/javascript';                            //import jquery
 document.getElementsByTagName('head')[0].appendChild(script);
 var url = window.location.href; //get current url
 
-if(url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART"){ //check to make sure the user is on the correct page
-    iframe = $("#ptifrmtgframe").contents() //creates a variable that references to the iframe
+function grabProfessorsName() {
+    if (url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART") { //check to make sure the user is on the correct page
+        iframe = $("#ptifrmtgframe").contents(); //creates a variable that references to the iframe
+        var instructorName = iframe.find("span[id*=MTG_INSTR");
 
+    }
 }
