8f571a4fe0e5e145ab13868930345ef2a32474b3
Justin <juol3453@colorado.edu>
2015-04-11 02:15:41 -0600

    ready for debugging

--- SearchAndRescue.js
+++ SearchAndRescue.js
@@ -3,43 +3,50 @@
  */
 
 var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';                            //import jquery
 document.getElementsByTagName('head')[0].appendChild(script);
 //var url = window.location.href; //get current url
 var professorsOnPage = new Array();
 var i = 0;
-iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
+function getIframe () {
+    return $("#ptifrmtgtframe").contents();
+}
+//iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
 function grabProfessorsName() {
+    var iframe = getIframe();
     //if (url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART") { //check to make sure the user is on the correct page
 
         $.each(iframe.find("span[id*=MTG_INSTR]"),
             function() {
                 if($(this.text) != "Staff"){
-                    professorsOnPage[i] = $(this).text;
+                    professorsOnPage[i] = "test" + $(this).text +"test" ;
                     i++;
                 }
-
-                //store in array
-                //modify fist last to first-last
-                //append to url "cufcq/instructors/"
         } )
 
         //var instructorName = iframe.find("span[id*=MTG_INSTR");
         for(j=0;j<professorsOnPage.length;j++){
             for(k=0;k<(professorsOnPage[j].length-1);k++){
-                if((professorsOnPage[j])[k] == " "){
-                    (professorsOnPage[j])[k] = "-"
+                if((professorsOnPage[j])[k] = = " "){
+                   (professorsOnPage[j])[k] = "-"
                 }
             }
             console.log(professorsOnPage[j]);
         }
+return 0;
 
-        //for each(function{regex})
 }
 //grabProfessorsName();
-element.addEventListener("DOMNodeInserted", function (ev) {
-    if(iframe.(document.getElementById("DERIVED_REGFRM1_TITLE1")) == "Search Results") {
-        grabProfessorsName();
-    }
-})
+$("iframe")[0].addEventListener("load", function () {
+    console.log("yoooooooo")
+    getIframe()[0].addEventListener("DOMSubtreeModified", function (ev) {
+        console.log("Iframe Subtree");
+        if(iframe[0].getElementById("DERIVED_REGFRM1_TITLE1").text == "Search Results") {
+            grabProfessorsName();
+            console.log("you are on the right page");
+        }
+    }, false);
+});
+
+//win0divSSR_CLSRSLT_WRK_GROUPBOX3$0"
