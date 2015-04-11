780a98f245a1860417747e9ff50416b7ef341d08
Justin <juol3453@colorado.edu>
2015-04-10 23:54:26 -0600

    bug fix

--- SearchAndRescue.js
+++ SearchAndRescue.js
@@ -9,9 +9,10 @@ document.getElementsByTagName('head')[0].appendChild(script);
 //var url = window.location.href; //get current url
 var professorsOnPage = new Array();
 var i = 0;
+iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
 function grabProfessorsName() {
     //if (url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART") { //check to make sure the user is on the correct page
-        iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
+
         $.each(iframe.find("span[id*=MTG_INSTR]"),
             function() {
                 if($(this.text) != "Staff"){
@@ -38,7 +39,7 @@ function grabProfessorsName() {
 }
 //grabProfessorsName();
 element.addEventListener("DOMNodeInserted", function (ev) {
-    if(document.getElementById("DERIVED_REGFRM1_TITLE1")) {
+    if(iframe.(document.getElementById("DERIVED_REGFRM1_TITLE1")) == "Search Results")) {
         grabProfessorsName();
     }
 })
