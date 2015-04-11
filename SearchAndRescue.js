8ca3d8efc847f2095b7b6477ec1119db224a3f6e
simmeringc <connersimmering@gmail.com>
2015-04-11 08:38:13 -0600

    Run JS on Website

--- SearchAndRescue.js
+++ SearchAndRescue.js
@@ -1,47 +1,97 @@
-console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+console.log("SearchAndRescue.js Running");
 
 /**
  * Created by justin on 4/10/15.
  */
 
 var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';                            //import jquery
 document.getElementsByTagName('head')[0].appendChild(script);
 //var url = window.location.href; //get current url
 var professorsOnPage = new Array();
 var i = 0;
-iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
+instructors = pullJSON();
+function getIframe() {
+    return $("#ptifrmtgtframe").contents();
+}
+//iframe = $("#ptifrmtgtframe").contents(); //creates a variable that references to the iframe
 function grabProfessorsName() {
+    var iframe = getIframe();
     //if (url == "https://portal.prod.cu.edu/psp/epprod/UCB2/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART") { //check to make sure the user is on the correct page
 
         $.each(iframe.find("span[id*=MTG_INSTR]"),
             function() {
-                if($(this.text) != "Staff"){
-                    professorsOnPage[i] = $(this).text;
+                if($(this).text() != "Staff"){
+                    professorsOnPage[i] = $(this).text();
                     i++;
                 }
+        } );
 
-                //store in array
-                //modify fist last to first-last
-                //append to url "cufcq/instructors/"
-        } )
 
         //var instructorName = iframe.find("span[id*=MTG_INSTR");
         for(j=0;j<professorsOnPage.length;j++){
-            for(k=0;k<(professorsOnPage[j].length-1);k++){
-                if((professorsOnPage[j])[k] == " "){
-                    (professorsOnPage[j])[k] = "-"
-                }
-            }
+            professorsOnPage[j] = professorsOnPage[j].replace(" ", "-");
+            //for(k=0;k<(professorsOnPage[j].length-1);k++){
+            //    if((professorsOnPage[j])[k] == " "){
+            //       (professorsOnPage[j])[k] = "-"
+            //    }
+            //}
             console.log(professorsOnPage[j]);
         }
+return 0;
 
-        //for each(function{regex})
+}
+function pullJSON(){
+    var instructors={};
+    request = $.getJSON("../instructors.json", function(data) { console.log(data); instructors = data; })
+    $.when(request).then(function(data, textStatus, jqXHR) {
+
+    })
+    return instructors;
 }
 //grabProfessorsName();
-element.addEventListener("DOMNodeInserted", function (ev) {
-    if(iframe.(document.getElementById("DERIVED_REGFRM1_TITLE1")) == "Search Results") {
-        grabProfessorsName();
-    }
-})
+$("iframe")[0].addEventListener("load", function () {
+    iframe = getIframe();
+    console.log("yoooooooo");
+    iframe[0].addEventListener("DOMSubtreeModified", function (ev) {
+        //console.log("Iframe Subtree");
+
+        if(iframe[0].getElementById("DERIVED_REGFRM1_TITLE1").innerHTML == null) {
+            console.log("not on the right page");
+        }
+        else if(iframe[0].getElementById("DERIVED_REGFRM1_TITLE1").innerHTML == "Search Results") {
+            grabProfessorsName();
+            console.log("you are on the right page");
+
+        }
+    }, false);
+});
+
+//add rows
+$.each(iframe.find("[id*=win0divSSR_CLSRCH_MTG1]"),
+    function() {
+        //$(document).ready(function(){
+        $(this).find('th').eq(7).after('<td id="instrHead">Instructor Score</td>');
+
+//       function ImportStyleFile () {
+            var linkTag = document.createElement ("link");
+            linkTag.href = "stylesheet.css";
+            linkTag.rel = "stylesheet";
+            var head = document.getElementsByTagName ("instrHead")[0];
+            head.appendChild (linkTag);
+//        }
+
+    });
+$.each(iframe.find("[id*=win0divSSR_CLSRCH_MTG1]"),
+    function() {
+        //$(document).ready(function(){
+        $(this).find('td').eq(8).after('<td id="instrBody">1.2/6.0</td>');
+
+    });
+
+
+
+//win0divSSR_CLSRSLT_WRK_GROUPBOX3$0"
+
+//win0divSSR_CLSRSLT_WRK_GROUPBOX3$0"
